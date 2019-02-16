2425c197dc420ed04f3f162dceffb491295120a0
Jamie Git Git <justicehopelove@yahoo.com>
2019-02-15 21:53:17 -0800

    finished functions

--- build.py
+++ build.py
@@ -28,49 +28,39 @@ list_of_pages = [
 
 #functions
 def main():
-    print("maine xecuted") ##
     create_full_base()
     #assemble each page 
     for page in list_of_pages:
         title= page["title"]
-        base= function_2(title)
-        function_3(title, base)    
-    print("finished")##        
+        base= assign_base(title)
+        assemble_page(title, base)           
         
  
-# Create full base with header and footer 1
-    #no return, no arguments       
+# Create full base with header and footer    
 def create_full_base():
-    print("function 1") ##
     #open basic base
     base_template = open("templates/base.html").read()
     # Read in the header and footer
     header_footer = open("templates/headerfooter.html").read()
-    # Use the string replace
+    # Add header and footer to basic base
     full_base = base_template.replace("{{content}}", header_footer)
     open("templates/fullbase.html", "w+").write(full_base)     
        
-# assign base 2
-        #argument is title, return base
-def function_2(page_name):
-    print("function 2") ##
+# Chooses correct base for each page
+def assign_base(page_name):
     base = None
     #index gets basic base, others get full base    
     if page_name == "index":
         base = open("templates/base.html").read()
-        print("base is open") ##
     else:
         base = open("templates/fullbase.html").read()
-        print("full base is open") ##
     return base
             
-#replace placeholder in each page with content 3
-            # argument is title, base. no return
-def function_3(page_name, page_template):
-    print("function 3") ##
-    # Read in the content of each HTML page
+#replace placeholder in each page with the page's content
+def assemble_page(page_name, page_template):
+    # Open the content of each HTML page
     content = open("content/" + page_name + ".html").read()
-    # Use the string replace
+    # place content in template
     finished_page = page_template.replace("{{content}}", content)
     open("docs/" + page_name + ".html", "w+").write(finished_page)
         
